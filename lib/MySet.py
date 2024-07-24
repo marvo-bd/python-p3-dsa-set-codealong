331543581ee2814f5939ec5fe8426ad7dc515e03
Marvin Kabira <marvobd@gmail.com>
2024-07-24 16:42:57 +0300

    lab completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,32 @@
 class MySet:
+    def __init__(self, initial_list=None):
+        '''Initialize the set with a list.'''
+        self.dictionary = {}
+        if initial_list:
+            for item in initial_list:
+                self.dictionary[item] = True
 
-    pass
+    def add(self, item):
+        '''Add an item to the set.'''
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        '''Remove an item from the set.'''
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        '''Check if the set contains an item.'''
+        return item in self.dictionary
+
+    def size(self):
+        '''Return the number of items in the set.'''
+        return len(self.dictionary)
+
+    def clear(self):
+        '''Clear all items from the set.'''
+        self.dictionary.clear()
+
+    def __str__(self):
+        '''Return a string representation of the set.'''
+        return 'MySet: {' + ','.join(map(str, self.dictionary.keys())) + '}'
